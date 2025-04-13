84d310e062a3dc8e5424d7693c24830261aa6979
grismane <kalmarauder@gmail.com>
2025-04-12 20:38:33 -0400

    B: Created circleshape.py and player.py

--- main.py
+++ main.py
@@ -3,25 +3,44 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-    # initialize pygame
-    pygame.init()
 
+    # CLI outputs
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # initialize pygame
+    pygame.init()
+
     # get a new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # initialize the game clock
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # create the game loop
-    countera = 0
-    while countera < 10000:
+    while True:
+        # prevent infinite looping by making the close button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(0x000000)
-        countera += 1
+
+        # draw the player object
+        player.draw(screen)
+
+        # update the image with the current state of the game
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,25 @@
+
+# import the CircleShape class from circleshape to be the parent class for Player
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+import pygame
+
+# Player class inherits from CircleShape
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        # override the draw method of CircleShape, so that it looks like Triangle that represents the player and not a circle
+        # pygame.draw.polygon takes a screen object, a colour, a list of points, a line width
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
